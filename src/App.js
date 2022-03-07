c0dbf9c90c24075109abad8df76e0df9c99d09be
Zain041 <53551255+Zain041@users.noreply.github.com>
2022-03-08 03:41:16 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import tw from 'twin.macro';
+import './App.css';
+const Four = tw.div`bg-blue-500 rounded-full`;
+function App() {
+  return (
+    <div className="App">
+      
+      <Four>haaaa lulya</Four>
+    </div>
+  );
+}
+
+export default App;
